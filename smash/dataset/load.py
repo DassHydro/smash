5f9a07605fa51dca48bad45313d5c951479786c7
francois <francois.colleoni@inrae.fr>
2022-11-07 15:00:05 -0800

    ENC: quick enc on load_dataset * Modified 	- load.py: Add DATASET_NAME constant and check lower() name

--- smash/dataset/load.py
+++ smash/dataset/load.py
@@ -8,14 +8,18 @@ __all__ = ["load_dataset"]
 
 DATASET_PATH = os.path.dirname(os.path.realpath(__file__))
 
+DATASET_NAME = ["flwdir", "cance"]
+
 
 def load_dataset(name: str):
+
+    name = name.lower()
 
     if name == "flwdir":
 
         return os.path.join(DATASET_PATH, "France_flwdir.tif")
 
-    elif name.capitalize() == "Cance":
+    elif name == "cance":
 
         setup = smash.read_setup(os.path.join(DATASET_PATH, "Cance/setup_Cance.yaml"))
         mesh = smash.read_mesh(os.path.join(DATASET_PATH, "Cance/mesh_Cance.hdf5"))
@@ -33,4 +37,4 @@ def load_dataset(name: str):
 
     else:
 
-        raise ValueError(f"Unknown dataset '{name}'")
+        raise ValueError(f"Unknown dataset '{name}'. Choices: {DATASET_NAME}")
