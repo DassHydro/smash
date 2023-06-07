059aad171a394bd82a1618876ec16103f801f58b
Jay-Allemand Maxime <maxime.jay.allemand@hydris-hydrologie.fr>
2023-06-07 11:50:43 +0200

    Remove prcp_prefix from setup.

--- smash/solver/derived_type/mwd_setup.f90
+++ smash/solver/derived_type/mwd_setup.f90
@@ -16,7 +16,6 @@
 !%      ``read_prcp``              Read precipitation                                     (default: .false.)
 !%      ``prcp_format``            Precipitation format                                   (default: 'tif')
 !%      ``prcp_fast_access``       Fast access with absolute path (YY/MM/dd/)             (default: .false.)
-!%      ``prcp_prefix``            Precipitation filename prefix                          (default: '...')
 !%      ``prcp_conversion_factor`` Precipitation conversion factor                        (default: 1)
 !%      ``prcp_directory``         Precipiation directory path                            (default: '...')
 !%      ``read_pet``               Reap potential evapotranspiration                      (default: .false.)
@@ -126,7 +125,6 @@ module mwd_setup
         logical :: read_prcp = .false.
         character(lchar) :: prcp_format = "tif" !>f90w-char
         logical :: prcp_fast_access = .false.
-        character(lchar) :: prcp_prefix = "..." !>f90w-char
         real(sp) :: prcp_conversion_factor = 1._sp
         character(lchar) :: prcp_directory = "..." !>f90w-char
 
