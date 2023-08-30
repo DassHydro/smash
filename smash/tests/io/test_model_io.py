06907ffc105bd36bfdbb73d577cd51556ad6e4b7
ngo-nghi-truyen.huynh <ngo-nghi-truyen.huynh@inrae.fr>
2023-08-31 00:45:25 +0200

    FIX PR: fix git workflow

--- smash/tests/io/test_model_io.py
+++ smash/tests/io/test_model_io.py
@@ -18,7 +18,7 @@ def generic_model_io(**kwargs) -> dict:
     model_rld = smash.io.read_model("tmp_model.hdf5")
 
     res = {
-        "model_io." + attr: np.array(dir(getattr(model_rld, attr)))
+        "model_io." + attr: np.array(dir(getattr(model_rld, attr))).astype("S")
         for attr in MODEL_PUBLIC_ATTRS
     }
 
@@ -29,7 +29,5 @@ def test_model_io():
     res = generic_model_io()
 
     for key, value in res.items():
-        value = value.astype("S")
-
         # % Check all values read from model
         assert np.array_equal(value, pytest.baseline[key][:]), key
