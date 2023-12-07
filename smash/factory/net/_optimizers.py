FIX: clean optimizer file and fix getting gradient for new structures

smash/factory/net/_optimizers.py
@@ -29,7 +29,6 @@ class SGD:
         if self.w_updt is None:
             self.w_updt = np.zeros(np.shape(w))
 
-        # Use momentum if set
         self.w_updt = self.momentum * self.w_updt + (1 - self.momentum) * grad_wrt_w
 
         return w - self.learning_rate * self.w_updt
@@ -82,9 +81,9 @@ class Adam:
         m_hat = self.m / (1 - self.b1)
         v_hat = self.v / (1 - self.b2)
 
-        self.w_updt = self.learning_rate * m_hat / (np.sqrt(v_hat) + self.eps)
+        w_updt = self.learning_rate * m_hat / (np.sqrt(v_hat) + self.eps)
 
-        return w - self.w_updt
+        return w - w_updt
 
 
 class Adagrad:
@@ -103,18 +102,20 @@ class Adagrad:
     def __init__(self, learning_rate: float = 0.01, eps=1e-8, **unknown_options):
         self.learning_rate = learning_rate
 
-        self.G = None  # Sum of squares of the gradients
+        self.g = None  # Sum of squares of the gradients
         self.eps = eps
 
     def update(self, w: np.ndarray, grad_wrt_w: np.ndarray):
-        if self.G is None:
-            self.G = np.zeros(np.shape(w))
+        if self.g is None:
+            self.g = np.zeros(np.shape(w))
 
         # Add the square of the gradient of the loss function at w
-        self.G += np.power(grad_wrt_w, 2)
+        self.g += np.power(grad_wrt_w, 2)
 
         # Adaptive gradient with higher learning rate for sparse data
-        return w - self.learning_rate * grad_wrt_w / np.sqrt(self.G + self.eps)
+        w_updt = self.learning_rate * grad_wrt_w / np.sqrt(self.g + self.eps)
+
+        return w - w_updt
 
 
 class RMSprop:
@@ -141,15 +142,17 @@ class RMSprop:
     ):
         self.learning_rate = learning_rate
 
-        self.Eg = None  # Running average of the square gradients at w
+        self.eg = None  # Running average of the square gradients at w
         self.eps = eps
         self.rho = rho
 
     def update(self, w: np.ndarray, grad_wrt_w: np.ndarray):
-        if self.Eg is None:
-            self.Eg = np.zeros(np.shape(grad_wrt_w))
+        if self.eg is None:
+            self.eg = np.zeros(np.shape(grad_wrt_w))
 
-        self.Eg = self.rho * self.Eg + (1 - self.rho) * np.power(grad_wrt_w, 2)
+        self.eg = self.rho * self.eg + (1 - self.rho) * np.power(grad_wrt_w, 2)
 
         # Divide the learning rate for a weight by a running average of the magnitudes of recent gradients for that weight
-        return w - self.learning_rate * grad_wrt_w / np.sqrt(self.Eg + self.eps)
+        w_updt = self.learning_rate * grad_wrt_w / np.sqrt(self.eg + self.eps)
+
+        return w - w_updt