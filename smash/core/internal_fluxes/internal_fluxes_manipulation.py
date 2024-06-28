c793fde45abc3386ae745561b3154bbe4cc45916
Apolline Elbaz <aelbaz@localhost.localdomain>
2024-06-28 17:47:13 +0200

    fix pb of size of internal fluxes

--- smash/core/internal_fluxes/internal_fluxes_manipulation.py
+++ smash/core/internal_fluxes/internal_fluxes_manipulation.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 def transform_internal_fluxes_to_dict(internal_fluxes, module_dictonnary, structure_name):
-    internal_fluxes = dict(
-                zip(module_dictonnary[structure_name], internal_fluxes)
-            )
+    internal_fluxes = {key: internal_fluxes[..., i] for i, key in enumerate(module_dictonnary[structure_name])}
     return internal_fluxes
+
