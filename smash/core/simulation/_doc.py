720b77c9a63762709afe925ad84e0f989de77b78
asjeb <151923475+asjeb@users.noreply.github.com>
2024-07-11 16:05:45 +0200

    Update smash/core/simulation/_doc.py
    Co-authored-by: Francois Colleoni <110899888+inoelloc@users.noreply.github.com>

--- smash/core/simulation/_doc.py
+++ smash/core/simulation/_doc.py
@@ -510,14 +510,6 @@ RETURN_OPTIONS_BASE_DOC = {
         Whether to return simulated discharge on the whole domain for specific time steps.
         """,
     ),
-    "qt": (
-        """
-        `bool`, default False
-        """,
-        """
-        Whether to return the routage discharge on the whole domain for specific time steps.
-        """,
-    ),
     "iter_cost": (
         """
         `bool`, default False
