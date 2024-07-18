ec419839c632a325cf6adfb386fa36585473d67d
Apolline Elbaz <aelbaz@localhost.localdomain>
2024-07-18 14:39:04 +0200

    update baseline tests

--- smash/tests/core/simulation/test_internal_fluxes.py
+++ smash/tests/core/simulation/test_internal_fluxes.py
@@ -2,9 +2,38 @@ from __future__ import annotations
 
 import os
 
+import numpy as np
 import pytest
 
 import smash
+
+
+def generic_internal_fluxes(model_structure: list[smash.Model], **kwargs) -> dict:
+    res = {}
+
+    ncpu = min(5, max(1, os.cpu_count() - 1))
+
+    for model in model_structure:
+        # % There is no snow data for the Cance dataset.
+        # % TODO: Add a dataset to test snow module
+        if model.setup.snow_module_present:
+            continue
+
+        instance, ret = smash.forward_run(
+            model,
+            common_options={"verbose": False, "ncpu": ncpu},
+            return_options={
+                "internal_fluxes": True,
+            },
+        )
+
+        mask = model.mesh.active_cell == 1
+
+        for key, value in ret.internal_fluxes.items():
+            internal_flux = value[mask, :].flatten()[::15000]
+            res[f"internal_fluxes.{instance.setup.structure}.{key}"] = internal_flux
+
+    return res
 
 
 def run_internal_fluxes_option(model: smash.Model, custom_options) -> dict:
@@ -14,8 +43,14 @@ def run_internal_fluxes_option(model: smash.Model, custom_options) -> dict:
     )
     return res
 
+def test_run_internal_fluxes():
+    res = generic_internal_fluxes(pytest.model_structure)
 
-def test_run_internal_fluxes_option():
+    for key, value in res.items():
+        assert np.allclose(value, pytest.baseline[key][:], atol=1e-06, equal_nan=True), key
+
+
+def test_run_internal_fluxes_shapes():
     ncpu = min(5, max(1, os.cpu_count() - 1))
     custom_options = [
         # % Test custom options
@@ -72,7 +107,7 @@ def test_run_internal_fluxes_option():
             assert internal_fluxes[k].shape == (pytest.model.mesh.nrow, pytest.model.mesh.ncol, nmts)
 
 
-def optimize_internal_fluxes_option(model: smash.Model, custom_options) -> dict:
+def optimize_internal_fluxes_shapes(model: smash.Model, custom_options) -> dict:
     instance, res = smash.optimize(
         model,
         **custom_options,
@@ -80,7 +115,7 @@ def optimize_internal_fluxes_option(model: smash.Model, custom_options) -> dict:
     return res
 
 
-def test_optimize_internal_fluxes_option():
+def test_optimize_internal_fluxes_shapes():
     ncpu = min(5, max(1, os.cpu_count() - 1))
     custom_options = [
         # % Test custom optimize_options
@@ -100,7 +135,7 @@ def test_optimize_internal_fluxes_option():
         },
     ]
     for i, kwargs in enumerate(custom_options):
-        res = optimize_internal_fluxes_option(pytest.model, kwargs)
+        res = optimize_internal_fluxes_shapes(pytest.model, kwargs)
         assert res is not None
         internal_fluxes = res.internal_fluxes
 
