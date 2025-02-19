ad5fe9fb8a271c7e9608a4e0a7312e75b043f9ba
ngo-nghi-truyen.huynh <ngo-nghi-truyen.huynh@inrae.fr>
2025-02-19 15:18:48 +0100

    Remove autosummary sphinx extension

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -51,7 +51,6 @@ extensions = [
     "IPython.sphinxext.ipython_directive",
     "IPython.sphinxext.ipython_console_highlighting",
     "matplotlib.sphinxext.plot_directive",
-    "sphinx_autosummary_accessors",
 ]
 
 
