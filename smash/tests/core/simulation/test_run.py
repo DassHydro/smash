caab49d9e1d0ff850cffa247801267fa8b487013
ngo-nghi-truyen.huynh <ngo-nghi-truyen.huynh@inrae.fr>
2025-03-01 22:42:02 +0100

    Fix baseline

--- smash/tests/core/simulation/test_run.py
+++ smash/tests/core/simulation/test_run.py
@@ -21,8 +21,7 @@ def generic_forward_run(model_structure: list[smash.Model], **kwargs) -> dict:
 
         # % Hybrid forward hydrological model with NN
         if model.setup.n_layers > 0:
-            model.set_nn_parameters_weight(initializer="glorot_normal", random_state=11)
-            model.set_nn_parameters_weight([w*0.2 for w in model.get_nn_parameters_weight()])
+            model.set_nn_parameters_weight(initializer="normal", random_state=11)
 
         instance, ret = smash.forward_run(
             model,
