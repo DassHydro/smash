07775a498215a585fd6c914e24ccb989661012a2
Jay-Allemand Maxime <maxime.jay.allemand@hydris-hydrologie.fr>
2025-06-04 12:28:05 +0200

    Export des parametre en Geotiff Ajout du code epsg dans le mesh car il permet de calculer le crs et de renseigner les métadonnée du geotiff

--- smash/io/__init__.py
+++ smash/io/__init__.py
@@ -2,6 +2,7 @@ from smash.io.mesh import read_mesh, save_mesh
 from smash.io.model import read_model, save_model
 from smash.io.model_ddt import read_model_ddt, save_model_ddt
 from smash.io.setup import read_setup, save_setup
+from smash.io.parameters import export_parameters
 
 __all__ = [
     "read_mesh",
@@ -12,4 +13,5 @@ __all__ = [
     "save_model",
     "save_model_ddt",
     "save_setup",
+    "export_parameters",
 ]
