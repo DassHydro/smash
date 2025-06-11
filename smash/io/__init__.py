119b73c4925c7fbe129c415900902d10603805e7
Jay-Allemand Maxime <maxime.jay.allemand@hydris-hydrologie.fr>
2025-06-11 12:45:46 +0200

    rename parameters.py to export_parameters.py to be consistent with a futur pull request that will include import_parameter.py

--- smash/io/__init__.py
+++ smash/io/__init__.py
@@ -1,7 +1,7 @@
 from smash.io.mesh import read_mesh, save_mesh
 from smash.io.model import read_model, save_model
 from smash.io.model_ddt import read_model_ddt, save_model_ddt
-from smash.io.parameters import export_parameters
+from smash.io.export_parameters import export_parameters
 from smash.io.setup import read_setup, save_setup
 
 __all__ = [
