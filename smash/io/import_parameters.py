ad097f88b32431ff4b27f18100faa42b23309e0e
Jay-Allemand Maxime <maxime.jay.allemand@hydris-hydrologie.fr>
2025-08-21 16:31:51 +0200

    make check

--- smash/io/import_parameters.py
+++ smash/io/import_parameters.py
@@ -52,7 +52,7 @@ def import_parameters(model: Model, path_to_parameters: FilePath):
 
             mask = np.where(cropped_param > FEASIBLE_RR_PARAMETERS[param][0])
             cropped_param[mask] = DEFAULT_RR_PARAMETERS[param]
-            
+
             pos = np.argwhere(list_param == param).item()
             model.rr_parameters.values[:, :, pos] = cropped_param
 
