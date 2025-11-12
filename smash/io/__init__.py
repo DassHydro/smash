5961c20b05a0cce9a3ac918f271887d9bc48b08d
Francois Colleoni <colleoni.francois@gmail.com>
2025-11-12 10:30:19 +0100

    ENH: Clean save and read grid parameters
    - Change function name to be a bit more explicit that the function is saving/reading
    only grid parameters (i.e. rr_parameters and rr_initial_states)
    
    - Active cell mask is not save by default, only if required by the user. So that, the functions
    save and read are consistents.
    
    - Add a few tests to check if the functions run
    
    - Add to doc the two new API functions, save_grid_parameters and read_grid_parameters

--- smash/io/__init__.py
+++ smash/io/__init__.py
@@ -1,15 +1,16 @@
-from smash.io.export_parameters import export_parameters
 from smash.io.mesh import read_mesh, save_mesh
 from smash.io.model import read_model, save_model
 from smash.io.model_ddt import read_model_ddt, save_model_ddt
+from smash.io.parameters import read_grid_parameters, save_grid_parameters
 from smash.io.setup import read_setup, save_setup
 
 __all__ = [
-    "export_parameters",
+    "read_grid_parameters",
     "read_mesh",
     "read_model",
     "read_model_ddt",
     "read_setup",
+    "save_grid_parameters",
     "save_mesh",
     "save_model",
     "save_model_ddt",
