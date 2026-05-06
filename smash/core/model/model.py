- Fix : adjust tangent linear call - New input_derivatives parameters to pass the gradients of an external model to the adjoint of Smash: the full gradient of the model chain in therefore obtained : dj/dX=dj/dq*dq/dx. This fix issue for calibrating smash when coupled with the gamma routing model

smash/core/model/model.py
@@ -58,6 +58,7 @@ from smash.core.simulation.estimate.estimate import _multiset_estimate
 from smash.core.simulation.optimize._standardize import (
     _standardize_bayesian_optimize_args,
     _standardize_grad_mode,
+    _standardize_input_derivatives,
     _standardize_optimize_args,
 )
 from smash.core.simulation.optimize._tools import _set_control
@@ -2933,12 +2934,14 @@ class Model:
         mapping: str = "uniform",
         optimizer: str = "lbfgsb",
         grad_mode: str = "j",
+        input_derivatives: np.ndarray | None = None,
         optimize_options: dict[str, Any] | None = None,
         cost_options: dict[str, Any] | None = None,
         common_options: dict[str, Any] | None = None,
         return_options: dict[str, Any] | None = None,
     ):
         grad_mode, return_options = _standardize_grad_mode(grad_mode, return_options)
+        input_derivatives = _standardize_input_derivatives(input_derivatives, grad_mode, self)
 
         args_options = [
             deepcopy(arg) for arg in [optimize_options, cost_options, common_options, return_options]
@@ -2958,6 +2961,6 @@ class Model:
                 self.mesh,
             )
 
-        grad = _backward_run(self, grad_mode, *args)
+        grad = _backward_run(self, grad_mode, input_derivatives, *args)
 
         return grad

smash/core/simulation/optimize/_tools.py
@@ -389,11 +389,16 @@ def _get_parameters_q_b(
     parameters: ParametersDT,
     wrap_options: OptionsDT,
     wrap_returns: ReturnsDT,
+    input_derivatives,
 ) -> ParametersDT:
     parameters_b = parameters.copy()
     output_b = model._output.copy()
 
-    output_b.response.qac = np.float32(1.0)
+    # pass reverse derivative through th adjoin dependent output variable output_b (in-killed))
+    if input_derivatives is not None:
+        output_b.response.qac = input_derivatives
+    else:
+        output_b.response.qac = np.float32(1.0)
 
     wrap_forward_run_q_b(
         model.setup,

smash/core/simulation/optimize/optimize.py
@@ -1311,6 +1311,7 @@ def _gradient_free_optimize_problem(
 def _backward_run(
     model: Model,
     grad_mode: str = "j",
+    input_derivatives: np.ndarray | None = None,
     mapping: str = "uniform",
     optimizer: str | None = None,
     optimize_options: dict[str, Any] | None = None,
@@ -1358,7 +1359,7 @@ def _backward_run(
     if grad_mode == "j":
         parameters_b = _get_parameters_b(model, parameters, wrap_options, wrap_returns)
     elif grad_mode in ["q", "qt"]:
-        parameters_b = _get_parameters_q_b(model, parameters, wrap_options, wrap_returns)
+        parameters_b = _get_parameters_q_b(model, parameters, wrap_options, wrap_returns, input_derivatives)
 
     grad = parameters_b.control.x.copy()
     name = parameters_b.control.name.copy()